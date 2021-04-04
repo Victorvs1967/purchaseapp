44cbe4b292d4a1f94bc2c1b32d26b4617a03f8ac
Victors1967 <victorsmirnov@me.com>
2021-04-04 11:59:26 +0300

    create apps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from './item/item';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'purchaseapp';
+  text: string;
+  price: number = 0;
+
+  items: Item[] = [
+    { purchase: "Bread", done: false, price: 15.9 },
+    { purchase: "Butter", done: false, price: 60 },
+    { purchase: "Potater", done: true, price: 22.9 },
+    { purchase: "Chees", done: false, price: 310 },
+  ]
+  addItem(text: string, price: number): void {
+    if (text == null || text.trim() == '' || price == null) return;
+    this.items.push(new Item(text, price));
+  }
 }
